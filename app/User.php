73b85e3ebaf07ea057ed1ecd7bb4e0a746575eb2
Nabil Muhammad Firdaus <nabilftd@gmail.com>
2020-05-07 19:41:03 +0700

    chore: Serialize email in Mutator

--- app/User.php
+++ app/User.php
@@ -2,13 +2,15 @@
 
 namespace App;
 
+use App\Extra\Database\Concerns\SerializeEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable,
+        SerializeEmail;
 
     /**
      * The attributes that are mass assignable.
@@ -36,4 +38,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function setEmailAttribute($value)
+    {
+        $this->attributes['email'] = static::serializeEmail($value);
+    }
 }
